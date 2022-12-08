b4731c65e89690004f7ee97cc4e439fa3d5314dc
haixin-fang <1875934025@qq.com>
2022-12-08 14:51:23 +0800

    feat: 支持本地源码预览

--- test/src/utils/sourcemap.js
+++ test/src/utils/sourcemap.js
@@ -21,8 +21,9 @@ function getFileLink(str) {
 }
 
 function loadSourceMap(fileName) {
-  let file, env = process.env.NODE_ENV;
-  if (env == "development") {
+  let file,
+    env = process.env.NODE_ENV;
+  if (env == 'development') {
     file = getFileLink(fileName);
   } else {
     file = matchStr(fileName);
@@ -30,21 +31,46 @@ function loadSourceMap(fileName) {
   if (!file) return;
   return new Promise(resolve => {
     fetch(`http://localhost:8083/getmap?fileName=${file}&env=${env}`).then(response => {
-      resolve(response.json());
+      if (env == 'development') {
+        resolve(response.text());
+      } else {
+        resolve(response.json());
+      }
     });
   });
 }
 
-
 export const findCodeBySourceMap = async ({ fileName, line, column }, callback) => {
   let sourceData = await loadSourceMap(fileName);
   if (!sourceData) return;
-  let { sourcesContent, sources } = sourceData;
-  let consumer = await new sourceMap.SourceMapConsumer(sourceData);
-  let result = consumer.originalPositionFor({
-    line: Number(line),
-    column: Number(column)
-  });
+  let result, codeList;
+  if (process.env.NODE_ENV == 'development') {
+    let source = getFileLink(fileName),
+      isStart = false;
+    result = {
+      source,
+      line: line - 1, // 具体的报错行数
+      column, // 具体的报错列数
+      name: null,
+    };
+    codeList = sourceData.split('\n').filter(item => {
+      if (item.indexOf('<script') != -1 || isStart) {
+        isStart = true;
+        return item;
+      } else if (item.indexOf('</script') != -1) {
+        isStart = false;
+      }
+    });
+  } else {
+    let { sourcesContent, sources } = sourceData;
+    let consumer = await new sourceMap.SourceMapConsumer(sourceData);
+    result = consumer.originalPositionFor({
+      line: Number(line),
+      column: Number(column),
+    });
+    let code = sourcesContent[sources.indexOf(result.source)];
+    codeList = code.split('\n');
+  }
   // result结果
   // {
   //   "source": "webpack://myapp/src/views/HomeView.vue",
@@ -52,19 +78,23 @@ export const findCodeBySourceMap = async ({ fileName, line, column }, callback) 
   //   "column": 0, // 具体的报错列数
   //   "name": null
   // }
-  let code = sourcesContent[sources.indexOf(result.source)];
-  let codeList = code.split('\n');
-  var row = result.line,
+  let row = result.line,
     len = codeList.length - 1;
-  var start = row - 5 >= 0 ? row - 5 : 0, // 将报错代码显示在中间位置
+  let start = row - 5 >= 0 ? row - 5 : 0, // 将报错代码显示在中间位置
     end = start + 9 >= len ? len : start + 9; // 最多展示10行
   let newLines = [];
   let j = 0;
-  for (var i = start; i <= end; i++) {
+  for (let i = start; i <= end; i++) {
     j++;
-    newLines.push(`<div class="code-line ${i + 1 == row ? 'heightlight' : ''}" title="${i + 1 == row ? result.source : ''}">${j}. ${repalceAll(codeList[i])}</div>`);
+    newLines.push(
+      `<div class="code-line ${i + 1 == row ? 'heightlight' : ''}" title="${
+        i + 1 == row ? result.source : ''
+      }">${j}. ${repalceAll(codeList[i])}</div>`
+    );
   }
 
-  let innerHTML = `<div class="errdetail"><div class="errheader">${result.source} at line ${result.column}:${row}</div><div class="errdetail">${newLines.join('')}</div></div>`;
+  let innerHTML = `<div class="errdetail"><div class="errheader">${result.source} at line ${
+    result.column
+  }:${row}</div><div class="errdetail">${newLines.join('')}</div></div>`;
   callback(innerHTML);
 };
